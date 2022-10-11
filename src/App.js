fad92d89f84c287d8d9a00fef588d489d2120efe
pavas <pavel.vas@okwork.io>
2022-10-11 14:17:19 +0300

    123

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ import { Loading } from './Loading'
 const Posts = lazy(() => import(/* webpackChunkName: "Posts" */ './Posts'))
 const Users = lazy(() => import(/* webpackChunkName: "Users" */ './Users'))
 
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            // You can render any custom fallback UI
+            return null;
+        }
+
+        return this.props.children;
+    }
+}
+
 function App() {
   const [isOpen, setOpen] = useState(false)
 
@@ -17,9 +43,11 @@ function App() {
     <div className="container">
       <h1>Suspense for Data Fetching</h1>
 
-      <Suspense fallback={<Loading />}>
-        <Posts />
-      </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<Loading />}>
+            <Posts />
+          </Suspense>
+        </ErrorBoundary>
 
       <button onClick={() => setOpen(true)}>Show users</button>
 
